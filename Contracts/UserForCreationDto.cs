ba734e702ebc0c12e30c4b49550854623a0875bb
IvanRozb <ivanrozb4225@gmail.com>
2023-06-05 16:44:35 +0300

    Created User DTOs
    After adding related entities and services will be added ICollection and IEnumerable fields

---
+++ Contracts/UserForCreationDto.cs
@@ -0,0 +1,9 @@
+namespace Contracts;
+
+public class UserForCreationDto
+{
+    public string Username { get; set; } = "";
+    public string Email { get; set; } = "";
+    public string FirstName { get; set; } = "";
+    public string LastName { get; set; } = "";
+}
---
+++ Contracts/UserForUpdateDto.cs
@@ -0,0 +1,10 @@
+namespace Contracts;
+
+public class UserForUpdateDto
+{
+    public Guid Id { get; set; }
+    public string Username { get; set; } = "";
+    public string Email { get; set; } = "";
+    public string FirstName { get; set; } = "";
+    public string LastName { get; set; } = "";
+}
