a2237a04f8d07e7488c952f5eb491eba62178328
IvanRozb <ivanrozb4225@gmail.com>
2023-06-05 18:57:35 +0300

    Added UserService and renamed Delete to Remove in IUserRepository.cs

--- Domain/Repositories/IUserRepository.cs
+++ Domain/Repositories/IUserRepository.cs
@@ -7,5 +7,5 @@ public interface IUserRepository
     Task<IEnumerable<User>> GetAllAsync(CancellationToken cancellationToken = default);
     Task<User> GetByIdAsync(Guid userId, CancellationToken cancellationToken = default);
     void Insert(User user);
-    void Delete(User user);
+    void Remove(User user);
 }
