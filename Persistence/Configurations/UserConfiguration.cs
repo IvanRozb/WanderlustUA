43cbcbb980ecf47e441c39854f9b95e525cfd77a
IvanRozb <ivanrozb4225@gmail.com>
2023-06-06 17:57:02 +0300

    Updated to real name of table in UserConfiguration

--- Persistence/Configurations/UserConfiguration.cs
+++ Persistence/Configurations/UserConfiguration.cs
@@ -8,7 +8,7 @@ internal sealed class UserConfiguration : IEntityTypeConfiguration<User>
 {
     public void Configure(EntityTypeBuilder<User> builder)
     {
-        builder.ToTable(nameof(User));
+        builder.ToTable("Users");
 
         builder.HasKey(user => user.Id);
 
