ec3ab366dc0c1d5dcb3a81ad306c65d94aa210f5
IvanRozb <ivanrozb4225@gmail.com>
2023-06-06 16:30:22 +0300

    Added all services, cors and controllers

--- Web/Program.cs
+++ Web/Program.cs
@@ -1,24 +1,59 @@
+using Domain.Repositories;
+using Persistence.Repositories;
+using Services;
+using Services.Abstractions;
+using Web.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddApplicationPart(typeof(Presentation.AssemblyReference).Assembly);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IServiceManager, ServiceManager>();
+builder.Services.AddScoped<IRepositoryManager, RepositoryManager>();
+
+builder.Services.AddTransient<ExceptionHandlingMiddleware>();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("DevCors", policyBuilder =>
+    {
+        policyBuilder.WithOrigins("http://localhost:3000/")
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials();
+    });
+    options.AddPolicy("ProdCors", policyBuilder =>
+    {
+        policyBuilder.WithOrigins("https://awfapp/")
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials();
+    });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseCors("DevCors");
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
+else
+{   
+    app.UseCors("ProdCors");
+    app.UseHttpsRedirection();
+}
 
 app.UseAuthorization();
+
+app.UseMiddleware<ExceptionHandlingMiddleware>();
 
 app.MapControllers();
 
