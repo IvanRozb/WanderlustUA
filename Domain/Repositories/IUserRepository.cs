929984e1528750416f348b8346bcadcf72b4da0f
IvanRozb <ivanrozb4225@gmail.com>
2023-06-06 18:07:08 +0300

    Updated all Tasks in User nullable

--- Domain/Repositories/IUserRepository.cs
+++ Domain/Repositories/IUserRepository.cs
@@ -4,8 +4,8 @@ namespace Domain.Repositories;
 
 public interface IUserRepository
 {
-    Task<IEnumerable<User>> GetAllAsync(CancellationToken cancellationToken = default);
-    Task<User> GetByIdAsync(Guid userId, CancellationToken cancellationToken = default);
-    void Insert(User user);
-    void Remove(User user);
+    Task<IEnumerable<User?>> GetAllAsync(CancellationToken cancellationToken = default);
+    Task<User?> GetByIdAsync(Guid userId, CancellationToken cancellationToken = default);
+    void Insert(User? user);
+    void Remove(User? user);
 }
--- Persistence/Repositories/UserRepository.cs
+++ Persistence/Repositories/UserRepository.cs
@@ -10,14 +10,14 @@ namespace Persistence.Repositories
 
         public UserRepository(RepositoryDbContext dbContext) => _dbContext = dbContext;
 
-        public async Task<IEnumerable<User>> GetAllAsync(CancellationToken cancellationToken = default) =>
+        public async Task<IEnumerable<User?>> GetAllAsync(CancellationToken cancellationToken = default) =>
             await _dbContext.Users.ToListAsync(cancellationToken);
 
-        public async Task<User> GetByIdAsync(Guid ownerId, CancellationToken cancellationToken = default) =>
-            await _dbContext.Users.FirstAsync(x => x.Id == ownerId, cancellationToken);
+        public async Task<User?> GetByIdAsync(Guid ownerId, CancellationToken cancellationToken = default) =>
+            await _dbContext.Users.FirstOrDefaultAsync(x => x != null && x.Id == ownerId, cancellationToken);
 
-        public void Insert(User owner) => _dbContext.Users.Add(owner);
+        public void Insert(User? owner) => _dbContext.Users.Add(owner);
 
-        public void Remove(User owner) => _dbContext.Users.Remove(owner);
+        public void Remove(User? owner) => _dbContext.Users.Remove(owner);
     }
 }
