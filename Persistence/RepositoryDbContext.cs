9a0c4a8752654aac9b4351223d30131b53e00712
IvanRozb <ivanrozb4225@gmail.com>
2023-06-06 17:56:26 +0300

    Removed explicit configuration in RepositoryDbContext

--- Persistence/RepositoryDbContext.cs
+++ Persistence/RepositoryDbContext.cs
@@ -6,24 +6,13 @@ namespace Persistence;
 
 public sealed class RepositoryDbContext : DbContext
 {    
-    private readonly IConfiguration? _configuration;
-    public RepositoryDbContext(IConfiguration configuration)
-    {
-        _configuration = configuration;
-    }
-    public RepositoryDbContext(DbContextOptions options)
+    public RepositoryDbContext(
+        DbContextOptions<RepositoryDbContext> options)
         : base(options)
     {
     }
 
     public DbSet<User> Users { get; set; }
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        if (!optionsBuilder.IsConfigured)
-            optionsBuilder.UseSqlServer(_configuration?.GetConnectionString("DefaultConnection"), 
-                builder => builder.EnableRetryOnFailure()
-            );
-    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder) =>
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(RepositoryDbContext).Assembly);
