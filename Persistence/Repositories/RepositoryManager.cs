96d9caca45c85579b194828eda9886539700392e
IvanRozb <ivanrozb4225@gmail.com>
2023-06-06 20:24:57 +0300

    Added AuthRepository to RepositoryManager

--- Persistence/Repositories/RepositoryManager.cs
+++ Persistence/Repositories/RepositoryManager.cs
@@ -5,15 +5,18 @@ namespace Persistence.Repositories
     public sealed class RepositoryManager : IRepositoryManager
     {
         private readonly Lazy<IUserRepository> _lazyUserRepository;
+        private readonly Lazy<IAuthRepository> _lazyAuthRepository;
         private readonly Lazy<IUnitOfWork> _lazyUnitOfWork;
 
         public RepositoryManager(RepositoryDbContext dbContext)
         {
             _lazyUserRepository = new Lazy<IUserRepository>(() => new UserRepository(dbContext));
+            _lazyAuthRepository = new Lazy<IAuthRepository>(() => new AuthRepository(dbContext));
             _lazyUnitOfWork = new Lazy<IUnitOfWork>(() => new UnitOfWork(dbContext));
         }
 
         public IUserRepository UserRepository => _lazyUserRepository.Value;
+        public IAuthRepository AuthRepository => _lazyAuthRepository.Value;
         public IUnitOfWork UnitOfWork => _lazyUnitOfWork.Value;
     }
 }
