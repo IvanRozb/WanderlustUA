0e4880eda98e99703a457d1a59a92b81d5cb54ff
IvanRozb <ivanrozb4225@gmail.com>
2023-06-06 16:29:16 +0300

    Added configuration to RepositoryDbContext

--- Persistence/RepositoryDbContext.cs
+++ Persistence/RepositoryDbContext.cs
@@ -1,17 +1,29 @@
 using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace Persistence;
 
 public sealed class RepositoryDbContext : DbContext
-{
+{    
+    private readonly IConfiguration? _configuration;
+    public RepositoryDbContext(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
     public RepositoryDbContext(DbContextOptions options)
         : base(options)
     {
     }
 
     public DbSet<User> Users { get; set; }
-
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+            optionsBuilder.UseSqlServer(_configuration?.GetConnectionString("DefaultConnection"), 
+                builder => builder.EnableRetryOnFailure()
+            );
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder) =>
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(RepositoryDbContext).Assembly);
