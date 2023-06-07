b9edd725bb22dd56c81b3405182976a2335de770
IvanRozb <ivanrozb4225@gmail.com>
2023-06-07 14:56:51 +0300

    Added TouristPlace entity

---
+++ Domain/Entities/TouristPlace.cs
@@ -0,0 +1,10 @@
+namespace Domain.Entities;
+
+public class TouristPlace
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = "";
+    public string Category { get; set; } = "";
+    public string Region { get; set; } = "";
+    public string Description { get; set; } = "";
+}
