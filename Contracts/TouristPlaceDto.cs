e540ed4462f77b13e3fdfdd3b1060e41cf48d8a0
IvanRozb <ivanrozb4225@gmail.com>
2023-06-07 14:57:03 +0300

    Added TouristPlace DTOs

---
+++ Contracts/TouristPlaceDto.cs
@@ -0,0 +1,10 @@
+namespace Contracts;
+
+public class TouristPlaceDto
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = "";
+    public string Category { get; set; } = "";
+    public string Region { get; set; } = "";
+    public string Description { get; set; } = "";
+}
---
+++ Contracts/TouristPlaceForUpdateDto.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Contracts;
+
+public class TouristPlaceForUpdateDto
+{
+    [Required(ErrorMessage = "Name is required")]
+    [StringLength(50, ErrorMessage = "Name can't be longer than 50 characters")]
+    public string Name { get; set; } = "";
+    
+    [Required(ErrorMessage = "Category is required")]
+    [StringLength(50, ErrorMessage = "Category can't be longer than 50 characters")]
+    public string Category { get; set; } = "";
+    
+    [Required(ErrorMessage = "Region is required")]
+    [StringLength(100, ErrorMessage = "Region can't be longer than 50 characters")]
+    public string Region { get; set; } = "";
+    
+    public string Description { get; set; } = "";
+}
